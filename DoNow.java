da0b15c14c7610c6e3a3b2126adda97adeae504a
jarritos <83789422+jarritos@users.noreply.github.com>
2021-09-20 09:16:34 -0700

    Add files via upload

---
+++ DoNow.java
@@ -0,0 +1,24 @@
+package test;
+
+public class DoNow {
+
+	public static void main(String[] args) {
+		
+	    eatBreakfast();
+	    brushTeeth();  
+	    goToSchool(); 
+	}
+
+	  public static void eatBreakfast(){
+	    System.out.println("Nom Nom"); 
+	    System.out.println("I'm eating food.");
+	  }
+	  public static void brushTeeth() {
+		  System.out.println("Brush Brush");
+		  System.out.println("Now my teeth are clean.");
+	  }
+	  public static void goToSchool() {
+		  System.out.println("Vroom Vroom");
+		  System.out.println("Now I'm at school.");
+	  }
+}
---
+++ PrintLn.java
@@ -0,0 +1,16 @@
+package test;
+
+public class PrintLn {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "Hello World" );	// Println automatically breaks the line currently being printed
+												// Where print only prints text without breaking after the message
+		System.out.println( "Class		- Contains code that can be executed" );
+		System.out.println( "Main Method	- Part of the Class that is automatically executed when called" );
+		System.out.println( "Editor		- Allows you to edit the code of your file" );
+		System.out.println( "Console		- Shows result of your code" );
+
+	}
+
+}
