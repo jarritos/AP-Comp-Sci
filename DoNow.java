9d1b3c6fa67d36b330c63a90bf8abf53a8f3b5b0
jarritos <83789422+jarritos@users.noreply.github.com>
2021-09-27 09:29:35 -0700

    Add files via upload

--- DoNow.java
+++ DoNow.java
@@ -3,22 +3,19 @@ package test;
 public class DoNow {
 
 	public static void main(String[] args) {
-		
-	    eatBreakfast();
-	    brushTeeth();  
-	    goToSchool(); 
+		drawSquare();
+	}
+	
+	public static void drawRow( ) {
+		System.out.println( "* * * * *" );
+	}
+	
+	public static void drawSquare( ) {
+		drawRow();
+		drawRow();
+		drawRow();
+		drawRow();
+		drawRow();
 	}
 
-	  public static void eatBreakfast(){
-	    System.out.println("Nom Nom"); 
-	    System.out.println("I'm eating food.");
-	  }
-	  public static void brushTeeth() {
-		  System.out.println("Brush Brush");
-		  System.out.println("Now my teeth are clean.");
-	  }
-	  public static void goToSchool() {
-		  System.out.println("Vroom Vroom");
-		  System.out.println("Now I'm at school.");
-	  }
 }
--- Tester.java
+++ Tester.java
@@ -5,5 +5,5 @@ public class Tester {
 	public static void main(String[] args) {
 		
 	}
-
+	
 }
